00d22b2c4936fd982297ef0bc90bc3feb3261bef
shefaveraja570-nexus <shefa.veraja.570@gmail.com>
2026-06-05 10:47:22 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'shefa-plus-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/estilos.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
