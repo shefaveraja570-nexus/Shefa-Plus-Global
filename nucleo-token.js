29cc0dfeae671a55fa75f5e32161e7078587c711
shefaveraja570-nexus <shefa.veraja.570@gmail.com>
2026-06-07 19:18:09 -0500

    Update nucleo-token.js

--- nucleo-token.js
+++ nucleo-token.js
@@ -11,3 +11,14 @@ class ActivoTokenizado {
     this.estado = 'ACTIVO_GENERANDO_VALOR';
   }
 }
+
+// Módulo: Expansión de Valor (Cálculo de Shefa)
+function calcularImpulsoEconomico(saldoActual, tasaCrecimiento, tiempoActivo) {
+  // Fórmula de interés compuesto alineada al crecimiento del ecosistema
+  const multiplicador = Math.pow((1 + tasaCrecimiento), tiempoActivo);
+  const nuevoSaldo = saldoActual * multiplicador;
+  
+  registrarCrecimientoEnHistorial(nuevoSaldo);
+  return nuevoSaldo;
+}
+
